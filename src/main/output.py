1a512b19dd81c6706d2b475a960ec12757acce78
shucshin <robinucscs@gmail.com>
2022-06-14 14:35:30 -0500

    Completed output.py

--- src/main/output.py
+++ src/main/output.py
@@ -1,16 +1,36 @@
 class Output:
+    """Clase para manejo de salida"""
     
     def makeAES(self, filename, encrypted):
-        #Get the filename without the extension and add '.aes'
+        """
+        Método que genera el archivo cifrado con la extensión .aes
+        
+        Parámetros: 
+            filename : nombre del archivo con la que vamos a generar el archivo .aes
+            encrypted : documento cifrado
+        """
         out = open(filename + '.aes', 'wb')
         out.write(encrypted)
     
     def makeFRG(self, filename, evals):
-        #Get the filename without the extension and add '.aes'
+        """
+        Método que genera el archivo con las n evaluaciones
+        con la extensión .frg
+        
+        Parámetros:
+            filename : nombre del archivo con la que vamos a generar el archivo .frg
+            evals : las n parejas evaluaciones del polinomio
+        """
         out = open(filename + '.frg', 'w')
         out.write(evals)
         
     def getOriginal(self, filename, decrypted):
-        #Escribe el contenido decifrado
+        """
+        Método que genera el archivo original
+        
+        Parámetros:
+            filename : nombre del archivo con la que vamos a generar el archivo descifrado
+            decrypted : documento claro
+        """
         out = open(filename, 'wb')
         out.write(decrypted)
