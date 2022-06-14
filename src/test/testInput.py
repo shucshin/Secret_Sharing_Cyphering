05db13a085f6e47cfb7b2bc6773ae16fcdf00c73
shucshin <robinucscs@gmail.com>
2022-06-14 15:39:00 -0500

    Completed testInput.py

--- src/test/testInput.py
+++ src/test/testInput.py
@@ -4,4 +4,23 @@ from main.input import *
 class TestInput(unittest.TestCase):
     """Prueba unittest para Polinomios"""
     
+    def testInputC(self):
+        inp = Input()
+        save_file = 'shamir.png'
+        num_eval = 10
+        min_pts = 3
+        clr_doc = 'shamir/Vaporwave.png'
+        fail = ['hello', 1, 999, 'hola']
+        self.assertRaises(TypeError, inp.checkExceptionsC, fail[0], num_eval, min_pts, clr_doc)
+        self.assertRaises(TypeError, inp.checkExceptionsC, save_file, fail[1], min_pts, clr_doc)
+        self.assertRaises(TypeError, inp.checkExceptionsC, save_file, num_eval, fail[2], clr_doc)
+        self.assertRaises(TypeError, inp.checkExceptionsC, save_file, num_eval, min_pts, fail[3])
+        
+    def testInputD(self):
+        inp = Input()
+        frg = 'shamir.png.frg'
+        aes = 'shamir.png.aes'
+        fail = ['sha', 'mir']
+        self.assertRaises(TypeError, inp.checkExceptionsD, fail[0], aes)
+        self.assertRaises(TypeError, inp.checkExceptionsD, frg, fail[1])
     
