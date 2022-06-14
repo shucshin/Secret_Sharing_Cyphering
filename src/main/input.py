7abba9c9ca5b35e9652c2a1a93378c0564ab4541
shucshin <robinucscs@gmail.com>
2022-06-14 14:18:55 -0500

    Completed input.py

--- src/main/input.py
+++ src/main/input.py
@@ -11,36 +11,48 @@ class Input:
         Método para leer los argumentos desde la términal, 
         obtener la contraseña indicada por el usuario,
         detectar posibles excepciones y extraer una
-        lista de de ellos.
+        lista de ellos.
         """
         save_file = sys.argv[2]
         num_eval = int(sys.argv[3])
         min_pts = int(sys.argv[4])
         clr_doc = sys.argv[5]
+        self.checkExceptionsC(save_file, num_eval, min_pts, clr_doc)
         password = getpass.getpass("Escriba la contraseña: ")
-        
-        
-        
-        
         return (save_file, num_eval, min_pts, clr_doc, password)
     
     def readD(self):
-        #Archivo con al menos t de n evaluaciones
+        """
+        Método para leer los argumentos desde la términal,
+        obtener el nombre del archivo con al menos t de n 
+        evaluaciones como .frg y al archivo cifrado como .aes, 
+        detectar posibles excepciones y extraer una lista de ellos.
+        """
         frg = sys.argv[2]
-        #Archivo cifrado
         aes = sys.argv[3]
+        self.checkExceptionsD(frg, aes)
         return (frg, aes)
     
-    def checkExceptionsC(self, save_file, num_eval, min_pts, clr_doc, password):
-        if(not save_file.endswith()):
-            raise TypeError("")
-        if(not isinstance(num_eval, int)):
-            raise TypeError("")
-        if(not isinstance(min_pts, int)):
-            raise TypeError("")
+    def checkExceptionsC(self, save_file, num_eval, min_pts, clr_doc):
+        """
+        Método para checar que los argumentos con la lectura de
+        bandera -d sean correctos.
+        """
+        if(not os.path.splitext(save_file)[1].startswith('.')):
+            raise TypeError("El archivo no tiene extensión")
+        if(not os.path.splitext(clr_doc)[1].startswith('.')):
+            raise TypeError("El archivo no tiene extensión")
+        if(not isinstance(num_eval, int) or num_eval < 3):
+            raise TypeError("El número de evaluaciones debe ser mayor que 2")
+        if(not isinstance(min_pts, int) or min_pts > num_eval):
+            raise TypeError("El número de puntos necesarios no puede ser mayor que el número de evaluaciones")
         
-    def checkExceptionsD(self, t_eval_file, n_pair_file):
-        if(not os.path.exists(t_eval_file)):
-            raise TypeError("")
-        if(not os.path.exists(n_pair_file)):
-            raise TypeError("")
+    def checkExceptionsD(self, frg, aes):
+        """
+        Método para checar que los argumentos con la lectura de
+        bandera -d sean correctos.
+        """
+        if(not os.path.exists(frg) or not frg.endswith('frg')):
+            raise TypeError("No está el archivo .frg")
+        if(not os.path.exists(aes) or not aes.endswith('aes')):
+            raise TypeError("No está el archivo .aes")
